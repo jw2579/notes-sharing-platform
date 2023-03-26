1071502920ddde321052a00c21fe6a9e567651f0
jw2579 <jw2579@cornell.edu>
2023-03-26 23:29:23 +0000

    empty db added

---
+++ db.php
@@ -0,0 +1,10 @@
+<?php
+
+    // use your own db authorization please
+    
+    
+    // if ($conn->connect_error) {
+    //     echo "no connection";
+    // } else {
+    //     echo "yes connection";
+    // }
